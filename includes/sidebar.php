129adf6e3d88ab71b23fde824ffd4e6890ac6a71
Zack-Lockhart <zack@goebelmediagroup.com>
2015-10-21 11:32:49 -0400

    Added Source Files

---
+++ includes/sidebar.php
@@ -0,0 +1,34 @@
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+              
+              	  <p class="centered"><a href="profile.html"><img src="assets/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
+              	  <h5 class="centered">Money.</h5>
+              	  	
+                  <li class="mt">
+                      <a class="active" href="/">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-desktop"></i>
+                          <span>Player Stats</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-cogs"></i>
+                          <span>Earning Log</span>
+                      </a>
+                  </li>
+              
+
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
